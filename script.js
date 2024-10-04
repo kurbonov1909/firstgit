f12e451a341e4a7ecc5b78aa535c6cf291f06b42
kurbonov1909 <kurbonovbnpz@gmail.com>
2024-10-04 08:20:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+
+function myfunction() {
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    document.getElementById('p').innerHTML = `Salom ${name} sizning yoshingiz ${age}da ekan. tanishganimdan xursandman`
+}
